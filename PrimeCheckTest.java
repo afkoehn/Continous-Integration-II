1927298b660c9aa1e0c5de26cba5335ff7100d71
afkoehn <120113053+afkoehn@users.noreply.github.com>
2025-01-19 21:24:45 +0100

    Fix Unit-Test

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -22,14 +22,18 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] primes = {3, 5, 7, 11, 13, 17, 19, 23, 29};
+    for (int prime : primes) {
+      assertEquals("Test prime number " + prime, true, PrimeCheck.isPrime(prime));
+    }
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] nonPrimes = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18};
+    for (int nonPrime : nonPrimes) {
+      assertEquals("Test non-prime number " + nonPrime, false, PrimeCheck.isPrime(nonPrime));
+    }
   }
 }
